ecea64bb394bd4eb7e1bc6d09ca658682c4eea57
siar1234 <siar019283@gmail.com>
2026-09-04 11:43:30 +0900

    chore: add MPL-2.0 license headers to source files

--- src/settings.h
+++ src/settings.h
@@ -1,3 +1,7 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 #ifndef CLOUD_CORE_SETTINGS_H
 #define CLOUD_CORE_SETTINGS_H
 #include <string>
